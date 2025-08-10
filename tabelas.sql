741fc365d6a69847398e18d2469bc2d9b250732f
Rafael-0809 <rafael.lavino.cabral@escola.pr.gov.br>
2025-08-09 22:04:11 -0300

    Tabelas criadas, tabela cliente com registros

---
+++ tabelas.sql
@@ -0,0 +1,24 @@
+-- Acabei perdendo os códigos de criação das tabela
+select * from clientes;
+
+insert into clientes values
+(default, 'Ana Silva', 'ana.silva@gmail.com', '2024-09-28'),
+(default, 'Bruno Costa', 'bruno.costa@email.com', '2024-06-15'),
+(default, 'Carla Dantas', 'carla.dantas@email.com', '2024-06-18'),
+(default, 'Daniel Fernandes', 'daniel.fernandes@email.com', '2024-06-22'),
+(default, 'Elisa Martins', 'elisa.martins@email.com', '2024-06-25'),
+(default, 'Felipe Guerra', 'felipe.guerra@email.com', '2024-06-28'),
+(default, 'Giovanna Rocha', 'giovanna.rocha@email.com', '2024-07-01'),
+(default, 'Hugo Oliveira', 'hugo.oliveira@email.com', '2024-07-05'),
+(default, 'Isabela Santos', 'isabela.santos@email.com', '2024-07-08'),
+(default, 'João Pereira', 'joao.pereira@email.com', '2024-07-12'),
+(default, 'Karina Alves', 'karina.alves@email.com', '2024-07-15'),
+(default, 'Lucas Guedes', 'lucas.guedes@email.com', '2024-07-18'),
+(default, 'Mariana Lima',	'mariana.lima@email.com', '2024-07-20'),
+(default, 'Nathan Vieira', 'nathan.vieira@email.com', '2024-07-23'),
+(default, 'Olívia Ribeiro', 'olivia.ribeiro@email.com', '2024-07-26'),
+(default, 'Pedro Almeida', 'pedro.almeida@email.com', '2024-07-29'),
+(default, 'Renata Castro', 'renata.castro@email.com', '2024-08-02'),
+(default, 'Sérgio Nunes', 'sergio.nunes@email.com', '2024-08-05'),
+(default, 'Tainá Freitas', 'taina.freitas@email.com', '2024-08-08'),
+(default, 'Victor Santos', 'victor.santos@email.com', '2024-08-11');
